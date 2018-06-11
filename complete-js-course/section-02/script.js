428a7436048ace7a5d14f505a3f08aa863bfc5a5
Koko Wahyu Prasetyo <kwprasetyo@gmail.com>
2018-06-11 12:00:58 +0700

    Section 2 - Lecture 13: Boolean Logic and Switch Statements

---
+++ complete-js-course/section-02/script.js
@@ -0,0 +1,97 @@
+// Lecture: Variables
+/* var name = "Tony";
+console.log(name);
+
+var lastName = "Stark";
+console.log(lastName);
+
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+ */
+
+ // Lecture: Variables Part 2
+ /*
+ var name = "Bruce";
+ var age = 26;
+
+//  console.log(name + age);
+//  console.log(age + age);
+
+ var job, isMarried;
+ 
+//  console.log(job);
+
+ job = "teacher";
+ isMarried = false;
+
+ console.log(name + " is a " + age + " years old "  + job + ". Is he married? " + isMarried + ".");
+
+ age = "twenty six";
+ job = "driver";
+
+ console.log(name + " is a " + age + " years old "  + job + ". Is he married? " + isMarried + ".");
+
+ var lastName = prompt("What is the last name?");
+ console.log(lastName);
+
+ alert(name + " is a " + age + " years old "  + job + ". Is he married? " + isMarried + ".");
+*/
+
+
+// Lecture: Operators
+/*
+var now = 2018;
+var birthYear = now - 33;
+
+birthYear = now - 33 * 2;
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+
+*/
+
+// ---------------------------
+// Lecture: IF/ELSE statements
+// ---------------------------
+/*
+var name = "John";
+var age = 26;
+var isMarried = "yes";
+
+if (isMarried === "yes") {
+    console.log(name + " is married!");
+} else {
+    console.log(name + " will hopefully marry soon :)");    
+}
+
+isMarried = false;
+
+if (isMarried) {
+    console.log("YES!");
+} else {
+    console.log("NO!");    
+}
+
+if (23 === "23") {
+    console.log("Something to print...");
+}
+*/
+
+
+// ---------------------------------
+// Lecture: Boolean logic and SWITCH
+// ---------------------------------
+
